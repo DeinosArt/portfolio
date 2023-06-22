990e3248d0df91ede95de230685cdf488e0d9eca
Deinos <deinos.art@gmail.com>
2023-06-22 18:21:38 +0200

    update for pythonanywhere

---
+++ pythonanywhere_launch.py
@@ -0,0 +1,6 @@
+from flask import Flask
+import serveur.controllers as controllers
+
+# global to run on https://help.pythonanywhere.com/pages/Flask/
+app = Flask(__name__)
+controllers.register_routes(app)
